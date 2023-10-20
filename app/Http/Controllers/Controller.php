cca8309276f43002f4a2e35bae743490a0044769
maruf.islam <maruf.islam@miaki.co>
2023-10-20 14:56:18 +0600

    feat:create common api response trait

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -2,11 +2,14 @@
 
 namespace App\Http\Controllers;
 
+use App\Traits\APIResponseTrait;
 use Illuminate\Foundation\Auth\Access\AuthorizesRequests;
 use Illuminate\Foundation\Validation\ValidatesRequests;
 use Illuminate\Routing\Controller as BaseController;
 
 class Controller extends BaseController
 {
-    use AuthorizesRequests, ValidatesRequests;
+    use AuthorizesRequests;
+    use ValidatesRequests;
+    use APIResponseTrait;
 }
